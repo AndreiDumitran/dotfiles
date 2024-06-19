5f163e2abbd429a9296dcbd55e996b1dabe42a9d
Andrei Dumitran <andrei.dumitran@AteliereMBP.local>
2024-06-19 22:34:42 +0300

    nvim-personal update

--- dot_config/nvim-personal/lua/custom/plugins/one-liners.lua
+++ dot_config/nvim-personal/lua/custom/plugins/one-liners.lua
@@ -3,4 +3,12 @@ return {
   { 'numToStr/Comment.nvim', opts = {} },
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
   { 'f-person/git-blame.nvim' },
+  { 'RRethy/vim-illuminate' },
+  {
+    'askfiy/lsp_extra_dim',
+    event = { 'LspAttach' },
+    config = function()
+      require('lsp_extra_dim').setup()
+    end,
+  },
 }

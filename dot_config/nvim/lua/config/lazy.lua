82fb80e8cb17dc460ed6a44fb3a20203b7196ddc
Andrei Dumitran <andrei.dumitran@AteliereMBP.local>
2024-10-02 21:41:54 +0300

    nvim configs

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -11,7 +11,6 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
-    { import = "lazyvim.plugins.extras.editor.harpoon2" },
 
     -- Languages
     { import = "lazyvim.plugins.extras.lang.clangd" },
@@ -57,6 +56,7 @@ require("lazy").setup({
 
     -- util
     { import = "lazyvim.plugins.extras.util.project" },
+    { import = "lazyvim.plugins.extras.editor.harpoon2" },
 
     { import = "lazyvim.plugins.extras.vscode" },
     { import = "plugins" },

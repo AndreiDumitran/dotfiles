e1d6ca7d0881075525eb91b5085ddd0fc26c1a4a
Andrei Dumitran <andrei.dumitran@AteliereMBP.local>
2025-02-18 12:42:42 +0200

    fix: fixed errors in nvim and wezterm

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -6,34 +6,3 @@ vim.cmd("autocmd BufEnter * set formatoptions-=cro")
 vim.cmd("autocmd BufEnter * setlocal formatoptions-=cro")
 vim.cmd("highlight FoldColumn guibg=black guifg=darkgrey")
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.foldingRange = {
-  dynamicRegistration = false,
-  lineFoldingOnly = true,
-}
-local language_servers = require("lspconfig").util.available_servers() -- or list servers manually like {'gopls', 'clangd'}
-for _, ls in ipairs(language_servers) do
-  require("lspconfig")[ls].setup({
-    capabilities = capabilities,
-    -- you can add other fields for setting up lsp server in this table
-  })
-end
-
-local border = {
-  { "┌", "FloatBorder" },
-  { "─", "FloatBorder" },
-  { "┐", "FloatBorder" },
-  { "│", "FloatBorder" },
-  { "┘", "FloatBorder" },
-  { "─", "FloatBorder" },
-  { "└", "FloatBorder" },
-  { "│", "FloatBorder" },
-}
-
--- Add border to the diagnostic popup window
-vim.diagnostic.config({
-  virtual_text = {
-    prefix = "■ ", -- Could be '●', '▎', 'x', '■', , 
-  },
-  float = { border = border },
-})
